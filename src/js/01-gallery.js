111870c9989a8f508b629770c594ccb2cc46ed7a
VoloshynaK <ko.voloshina@gmail.com>
2024-01-19 19:31:20 -0500

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+// Add imports above this linenpm remove
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const imgElMarkup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}"/>
+            
+        </a>
+    </li>`).join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", imgElMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
